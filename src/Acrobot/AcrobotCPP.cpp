80fd3674272b50abfd67eddc7e49fe261dcee279
Armando Herrera <anhydrous99@gmail.com>
2020-05-17 21:30:46 -0500

    almost finished Acrobot env

---
+++ src/Acrobot/AcrobotCPP.cpp
@@ -0,0 +1,62 @@
+//
+// Created by constexpr_dog on 5/17/20.
+//
+
+#include "AcrobotCPP.h"
+#include <functional>
+#include <algorithm>
+#include <cassert>
+#include <tuple>
+#include <cmath>
+
+AcrobotCPP::AcrobotCPP() : EnvCPP<float, int8_t, 6>() {}
+
+std::tuple<std::array<float, 6>, float, bool> AcrobotCPP::step(int8_t action) {
+    assert(0 <= action && action <= 3);
+    float torque = avail_torque[action];
+
+    // Add noise to the force action
+    if (torque_noise_max > 0.0f) {
+        std::uniform_real_distribution<float> tmp_dist(-torque_noise_max, torque_noise_max);
+        torque += tmp_dist(_ran_generator);
+    }
+}
+
+std::array<float, 6> AcrobotCPP::reset() {
+    std::generate(_state.begin(), _state.end(), [&](){ return dist(_ran_generator); });
+    return get_obs();
+}
+
+std::array<float, 6> AcrobotCPP::get_obs() {
+    float s0cos, s0sin, s1cos, s1sin;
+    sincosf(_state[0], &s0sin, &s0cos);
+    sincosf(_state[1], &s1sin, &s1cos);
+    return {s0cos, s0sin, s1cos, s1sin, _state[2], _state[3]};
+}
+
+bool AcrobotCPP::terminal() {
+    return ((-std::cos(_state[0]) - std::cos(_state[1] + _state[0])) > 1.0f);
+}
+
+std::valarray<float> AcrobotCPP::dsdt(const std::array<float, 4> &s, float a) {
+    auto [theta1, theta2, dtheta1, dtheta2] = s;
+
+    // Calculate the sine and cosine of theta1 and theta2
+    float stheta1, ctheta1, stheta2, ctheta2;
+    sincosf(theta1, &stheta1, &ctheta1);
+    sincosf(theta2, &stheta2, &ctheta2);
+
+    // Calculate d1
+    const float d1 = I2 + mllp2lclc + mllc2 * ctheta2;
+    const float d2 = I + mllc * ctheta2 + mlclc;
+    const float phi2 = glcm * std::sin(theta1 + theta2);
+    const float phi1 = gmlplc * stheta1 - mllc * dtheta2 * (2.0f * dtheta1 + dtheta2) * stheta2 + phi2;
+    const float ddtheta2 = (d1 * (phi2 - a) - d2 * phi1 + mllc * d1 * dtheta1 * dtheta1 * stheta2) /
+            (d2 * d2 - d1 * (mlclc + I));
+    const float ddtheta1 = -(d2 * ddtheta2 + phi1) / d1;
+    return {dtheta1, dtheta2, ddtheta1, ddtheta2, 0.0f};
+}
+
+void AcrobotCPP::calc_rk4(const std::array<float, 5> &y0, const std::array<float, 2> &t) {
+    
+}
---
+++ src/Acrobot/AcrobotCPP.h
@@ -0,0 +1,55 @@
+//
+// Created by constexpr_dog on 5/17/20.
+//
+
+#ifndef CPPGYM_ACROBOT_H
+#define CPPGYM_ACROBOT_H
+
+#include "../EnvCPP.h"
+#include <valarray>
+
+class AcrobotCPP : public EnvCPP<float, int8_t, 6> {
+    std::uniform_real_distribution<float> dist{-0.1, 0.1};
+
+    std::array<float, 6> get_obs();
+    bool terminal();
+    std::valarray<float> dsdt(const std::array<float, 4> &s, float a);
+    void calc_rk4(const std::array<float, 5> &y0, const std::array<float, 2> &t);
+public:
+    const float dt = 0.2f;
+    const float link_length = 1.0f;
+    const float link_mass = 1.0f;
+    const float link_com_pos = 0.5f;
+    const float link_moi = 1.0f;
+    const float max_vel_1 = 4.0f * M_PIf32;
+    const float max_vel_2 = 9.0f * M_PIf32;
+    const std::array<float, 3> avail_torque{-1.0f, 0.0f, 1.0f};
+    float torque_noise_max = 0.0f;
+    const bool book_or_nips = true; // set to false for nips
+
+    // Precalculations
+    const float m = link_mass;
+    const float l = link_length;
+    const float lc = link_com_pos;
+    const float I = link_moi;
+    const float g = 9.8f;
+    const float ll = l * l;
+    const float lclc = lc * lc;
+    const float mlclc = m * lclc;
+    const float llc = l * lc;
+    const float mllc = m * llc;
+    const float llc2 = llc * 2.0f;
+    const float mllc2 = llc2 * m;
+    const float lclc2 = lclc * 2.0f;
+    const float I2 = I * 2.0f;
+    const float glcm = g * lc * m;
+    const float gmlplc = g * m * (l + lc);
+    const float mllp2lclc = m * (ll + lclc2);
+
+    AcrobotCPP();
+    std::tuple<std::array<float, 6>, float, bool> step(int8_t action) override;
+    std::array<float, 6> reset() override;
+
+};
+
+#endif //CPPGYM_ACROBOT_H
